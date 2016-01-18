9d37ba5d26c14e4481ff6f32ab847df00e7234a8
Nicholas Stamas <nick@Nicholass-MacBook-Pro.local>
2016-01-18 13:50:31 -0500

    add support for alpha

--- ios/BlurView.h
+++ ios/BlurView.h
@@ -3,5 +3,6 @@
 @interface BlurView : UIView
 
 @property (nonatomic, copy) NSString *blurType;
+@property (nonatomic) float alpha;
 
 @end
--- ios/BlurView.m
+++ ios/BlurView.m
@@ -1,36 +1,47 @@
 #import "BlurView.h"
 
 @implementation BlurView {
-  UIVisualEffectView *_visualEffectView;
+    UIVisualEffectView *_visualEffectView;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    _visualEffectView = [[UIVisualEffectView alloc] init];
+    return [super initWithFrame:frame];
 }
 
 - (void)setBlurType:(NSString *)blurType
 {
-  if (_visualEffectView) {
-    [_visualEffectView removeFromSuperview];
-  }
+    //  if (_visualEffectView) {
+    //    [_visualEffectView removeFromSuperview];
+    //  }
+    
+    UIBlurEffect *blurEffect;
+    
+    if ([blurType isEqual: @"xlight"]) {
+        blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
+    } else if ([blurType isEqual: @"light"]) {
+        blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    } else if ([blurType isEqual: @"dark"]) {
+        blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    } else {
+        blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    }
+    
+    //  _visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    [_visualEffectView setEffect:blurEffect];
+}
 
-  UIBlurEffect *blurEffect;
-
-  if ([blurType isEqual: @"xlight"]) {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
-  } else if ([blurType isEqual: @"light"]) {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-  } else if ([blurType isEqual: @"dark"]) {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-  } else {
-    blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-  }
-
-  _visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+- (void)setAlpha:(float)value
+{
+    _visualEffectView.alpha = value;
 }
 
 -(void)layoutSubviews
 {
-  [super layoutSubviews];
-
-  _visualEffectView.frame = self.bounds;
-  [self insertSubview:_visualEffectView atIndex:0];
+    [super layoutSubviews];
+    _visualEffectView.frame = self.bounds;
+    [self insertSubview:_visualEffectView atIndex:0];
 }
 
 @end
